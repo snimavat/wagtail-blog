8bfc7e6365743e9122bbfb69eabe68a0e858784a
sudhir nimavat <sudhir@nimavat.me>
2017-02-04 19:36:46 +0530

    cleanup and configure to use mysql for production

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN mkdir /config
 RUN mkdir /app
 COPY ./config/requirements.txt /config
 RUN apk update
-RUN apk add build-base python-dev py-pip jpeg-dev zlib-dev
+RUN apk add bash build-base jpeg-dev zlib-dev
 ENV LIBRARY_PATH=/lib:/usr/lib
 RUN pip install --no-cache-dir -r /config/requirements.txt
 WORKDIR /app
--- app/lapsi/settings/production.py
+++ app/lapsi/settings/production.py
@@ -5,9 +5,20 @@ from .base import *
 DEBUG = False
 SECRET_KEY = 'x123gd1233556rsgtyu56twefty5redqwqwerh5tretr24%^$%^&$RE2e3qwDSDefref'
 
-ALLOWED_HOSTS = ['nimavat.me','www.nimavat.me']
+ALLOWED_HOSTS = ['nimavat.me','www.nimavat.me', 'localhost']
 
 STATIC_ROOT = "/static"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lapsi',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
 
 try:
     from .local import *
